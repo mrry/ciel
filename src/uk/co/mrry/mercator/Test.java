128ce4f63445c1738a5a03a836af2cbd41fb7e89
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2010-01-25 14:38:57 +0000

    Example commit to start off.

---
+++ src/uk/co/mrry/mercator/Test.java
@@ -0,0 +1,13 @@
+package uk.co.mrry.mercator;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!");
+	}
+
+}
