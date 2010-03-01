264de8f0ed8edb6191ef04fd0dadef3ed4b48311
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2010-03-01 21:12:39 +0000

    Data model to ORM.

--- src/python/mrry/mercator/master/datamodel.py
+++ src/python/mrry/mercator/master/datamodel.py
@@ -4,15 +4,19 @@ Created on Feb 26, 2010
 @author: derek
 '''
 
-import sqlalchemy.ext.declarative
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, Table, create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy import PickleType
 import simplejson
-from mrry.mercator.cloudscript.parser import CloudScriptParser
 from sqlalchemy.orm import relation
 from sqlalchemy.schema import ForeignKey
 
 Base = declarative_base()
+
+WORKER_STATUS_IDLE = 0
+WORKER_STATUS_BUSY = 1
+WORKER_STATUS_FAILED = 2
 
 class Worker(Base):
     
@@ -20,10 +24,71 @@ class Worker(Base):
     
     id = Column(Integer, primary_key=True)
     uri = Column(String, unique=True)
+    status = Column(Integer)
+
+task_input = Table('task_input', Base.metadata,
+                   Column('task_id', Integer, ForeignKey('task.id')),
+                   Column('datum_id', Integer, ForeignKey('datum.id')))
+
+task_output = Table('task_output', Base.metadata,
+                    Column('task_id', Integer, ForeignKey('task.id')),
+                    Column('datum_id', Integer, ForeignKey('datum.id')))
+
+class DataRepresentation(Base):
     
-    def __init__(self, uri):
-        self.uri = uri
+    __tablename__ = 'data_representation'
+    
+    id = Column(Integer, primary_key=True)
+    datum_id = Column(Integer, ForeignKey('datum.id'))
+    discriminator = Column(String(10))
+    
+    __mapper_args__ = {'polymorphic_on' : discriminator}
+    
+    def as_dict(self):
+        raise
         
+class Datum(Base):
+    
+    __tablename__ = 'datum'
+    
+    id = Column(Integer, primary_key=True)
+    size = Column(Integer)
+    
+    representations = relation(DataRepresentation, backref='datum')
+
+TASK_STATUS_BLOCKED = 0
+TASK_STATUS_RUNNABLE = 1
+TASK_STATUS_RUNNING = 2
+TASK_STATUS_COMPLETED = 3
+
+class Task(Base):
+    
+    __tablename__ = 'task'
+    
+    id = Column(Integer, primary_key=True)
+    workflow_id = Column(Integer, ForeignKey('workflow.id'))
+    args = Column(PickleType)
+    status = Column(Integer)
+
+    inputs = relation(Datum, secondary=task_input)
+    outputs = relation(Datum, secondary=task_output)
+
+TASK_ATTEMPT_STATUS_RUNNING = 0
+TASK_ATTEMPT_STATUS_COMPLETED = 1
+TASK_ATTEMPT_STATUS_FAILED = 2
+
+class TaskAttempt(Base):
+    
+    __tablename__ = 'task_attempt'
+    
+    id = Column(Integer, primary_key=True)
+    task_id = Column(Integer, ForeignKey('task.id'))
+    worker_id = Column(Integer, ForeignKey('worker.id'))
+    status = Column(Integer)
+    
+    task = relation(Task, backref='attempts')
+    worker = relation(Worker, backref='task_attempts')
+            
 class Workflow(Base):
     
     __tablename__ = 'workflow'
@@ -34,8 +99,8 @@ class Workflow(Base):
     
     tasks = relation(Task, order_by=Task.id, backref='workflow')
     
-    def __init__(self, text, context):
-        self.script_text = text
+    def __init__(self, script_text, script_context):
+        self.script_text = script_text
         self.script_context = script_context
         self.parsed_ast = None
         self.parsed_context = None
@@ -45,20 +110,70 @@ class Workflow(Base):
             self.parsed_context = simplejson.loads(self.script_context)
         return self.parsed_context
         
-    def ast(self, parser=CloudScriptParser()):
+    def ast(self, parser):
         if self.parsed_ast is None:
             self.parsed_ast = parser.parse(self.script_text)
         return self.parsed_ast
+
+class LocalFile(DataRepresentation):
     
-class Task(Base):
+    __mapper_args__ = {'polymorphic_identity' : 'local_file'}
     
-    __tablename__ = 'task'
+    worker_id = Column(Integer, ForeignKey('worker.id'))
+    worker = relation(Worker, backref='local_files')
+    filename = Column(String)
     
-    id = Column(Integer, primary_key=True)
-    workflow_id = Column(Integer, ForeignKey('workflow.id'))
+    def as_dict(self):
+        return {'type':'file', 'worker':self.worker.hostname, 'filename':self.filename}
     
-    inputs = relation(Data, order_by=Data.id, backref='task')
-    outputs = relation(Data, order_by=Data.id, backref)
+class WebFile(DataRepresentation):
     
-    def __init__(self, inputs):
-        pass
+    __mapper_args__ = {'polymorphic_identity' : 'web_file'}
+    
+    url = Column(String)
+    
+    def as_dict(self):
+        return {'type':'http', 'url':self.url}
+    
+class Regenerate(DataRepresentation):
+    
+    __mapper_args__ = {'polymorphic_identity' : 'regenerate'}
+    
+    task_id = Column(Integer, ForeignKey('task.id'))
+    task = relation(Task)
+    
+    def as_dict(self):
+        return {'type':'regenerate', 'task':'task_id'}
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+    
+if __name__ == '__main__':
+    
+
+    s = Session()
+    
+    w = Worker(uri='http://www.mrry.co.uk/')
+    s.add(w)
+
+    wf = Workflow("foo", "bar")
+    s.add(wf)
+    
+    d1 = Datum(size=1048576)
+    d2 = Datum(size=2097192)
+    s.add(d1)
+    s.add(d2)
+    
+    t = Task(inputs=[d1, d2])
+    s.add(t)
+
+    wf.tasks = [t]
+
+    s.commit()
+
+    s.close()
+    
+
+    
--- src/python/mrry/mercator/master/scheduler.py
+++ src/python/mrry/mercator/master/scheduler.py
@@ -8,6 +8,11 @@ from threading import Lock, Thread
 from Queue import Queue, Empty
 from uuid import uuid4
 from mrry.mercator.jobmanager.plugins import THREAD_TERMINATOR
+from mrry.mercator.master.datamodel import Session, Worker, TaskAttempt,\
+    TASK_ATTEMPT_STATUS_FAILED, TASK_STATUS_QUEUED, TASK_STATUS_COMPLETED,\
+    TASK_ATTEMPT_STATUS_COMPLETED, WORKER_STATUS_IDLE, TASK_STATUS_RUNNABLE,\
+    WORKER_STATUS_BUSY, TASK_STATUS_RUNNING
+from sqlalchemy.orm import eagerload
 import simplejson
 import httplib2
 import collections
@@ -29,31 +34,31 @@ class SchedulerProxy(plugins.SimplePlugin):
         
         self.bus.subscribe('add_worker', self.add_worker)
         self.bus.subscribe('remove_worker', self.remove_worker)
-        self.bus.subscribe('add_production_rule', self.add_production_rule)
-        self.bus.subscribe('add_failed_task', self.add_failed_task)
-        self.bus.subscribe('add_completed_task', self.add_completed_task)
+        self.bus.subscribe('add_task', self.add_task)
+        self.bus.subscribe('add_failed_task_attempt', self.add_failed_task_attempt)
+        self.bus.subscribe('add_completed_task_attempt', self.add_completed_task_attempt)
         
     def trigger_scheduler(self, reason):
         self.scheduler_invoke_queue.put(reason)
   
-    def add_worker(self, worker_id):
-        self.new_worker_queue.put((self.internal_scheduler.add_worker, worker_id))
+    def add_worker(self, worker):
+        self.input_queue.put((self.internal_scheduler.add_worker, (worker, )))
         self.trigger_scheduler('new_worker')
         
     def remove_worker(self, worker_id):
-        self.dead_worker_queue.put((self.internal_scheduler.remove_worker, worker_id))
+        self.input_queue.put((self.internal_scheduler.remove_worker, (worker_id, )))
         self.trigger_scheduler('dead_worker')
   
-    def add_production_rule(self, rule):
-        self.production_rule_queue.put((self.internal_scheduler.add_production_rule, rule))
+    def add_task(self, task):
+        self.input_queue.put((self.internal_scheduler.add_task, (task, )))
         self.trigger_scheduler('new_task')
         
-    def add_failed_task(self, task_id):
-        self.failed_task_queue.put((self.internal_scheduler.add_failed_task, task_id))
+    def add_failed_task_attempt(self, attempt_id):
+        self.input_queue.put((self.internal_scheduler.add_failed_task_attempt, (attempt_id, )))
         self.trigger_scheduler('task_failed')
         
-    def add_completed_task(self, task_id):
-        self.completed_task_queue.put((self.internal_scheduler.add_completed_task, task_id))
+    def add_completed_task_attempt(self, attempt_id, new_output_data):
+        self.input_queue.put((self.internal_scheduler.add_completed_task_attempt, (attempt_id, new_output_data)))
         self.trigger_scheduler('task_completed')
   
     def start(self):
@@ -78,8 +83,8 @@ class SchedulerProxy(plugins.SimplePlugin):
     def do_schedule(self):
         while True:
             try:
-                new_worker = self.new_worker_queue.get_nowait()
-                self.internal_scheduler.add_worker(new_worker)
+                method, args = self.input_queue.get_nowait()
+                method(*args)
             except Empty:
                 break
 
@@ -90,6 +95,8 @@ class SingleThreadedScheduler:
     def __init__(self, bus):
         
         self.bus = bus
+        
+        self.session = Session()
         
         # TODO: this should probably be a SQLite database.
         
@@ -111,82 +118,97 @@ class SingleThreadedScheduler:
         # List of (production rule, set of datum-id) tuples.
         self.blocked_list = []
     
-    def add_worker(self, worker_id):
-        self.idle_workers.add(worker_id)
+    def add_worker(self, worker):
+        self.session.add(worker)
         
     def remove_worker(self, worker_id):
-        try:
-            failed_task_id = self.busy_workers[worker_id]
-            del self.busy_workers[worker_id]
-            self.add_failed_task(failed_task_id)
-        except KeyError:
-            return
+        worker = self.session.query(Worker).get(worker_id)
+        
+        for attempt in worker.task_attempts:
+            self.add_failed_task_attempt(attempt.id)
+        
+        self.session.delete(worker)
         
         # TODO: see if this worker was the only source for a bunch of inputs, if so, may have to reproduce them.
         # This may make a bunch of tasks non-runnable.
         # This will probably require notions of (i) data being produced (by running tasks), and (ii) storing the production
         # rule in the data store.
     
-    def add_production_rule(self, rule):
-        dependency_list = rule.get_data_dependencies()
-        pending_dependency_list = [x for x in dependency_list if x not in self.available_data]
-        if len(pending_dependency_list == 0):
-            self.runnable_list.append(rule)
-        else:
-            self.blocked_list.append((rule, set(pending_dependency_list)))
+    def add_task(self, task):
+        self.session.add(task)
+#        
+#        dependency_list = rule.get_data_dependencies()
+#        pending_dependency_list = [x for x in dependency_list if x not in self.available_data]
+#        if len(pending_dependency_list == 0):
+#            self.runnable_list.append(rule)
+#        else:
+#            self.blocked_list.append((rule, set(pending_dependency_list)))
     
-    def add_failed_task(self, task_id):
-        task = self.running_tasks[task_id]
-        del self.running_tasks[task_id]
-        self.add_production_rule(task.rule)
+    def add_failed_task_attempt(self, task_attempt_id):
+        attempt = self.session.query(TaskAttempt).get(task_attempt_id)
+        task = attempt.task
         
-    def add_completed_task(self, task_id):
-        task = self.running_tasks[task_id]
-        del self.running_tasks[task_id]
-        del self.busy_workers[task.worker]
-        self.idle_workers.add(task.worker)
+        attempt.status = TASK_ATTEMPT_STATUS_FAILED
+        task.status = TASK_STATUS_QUEUED
         
-        newly_available_data = task.rule.get_data_outputs()
+    def add_completed_task_attempt(self, task_attempt_id, new_output_data):
+        attempt = self.session.query(TaskAttempt).get(task_attempt_id)
+        task = attempt.task
+        worker = attempt.worker
         
-        # TODO: replace with a smarter (tree-based) lookup structure for the
-        #       pending-dependency set.
-        #       (e.g. a map from pending output to sets).
-        still_blocked_list = []
-        for (blocked_rule, pending_dependency_set) in self.blocked_list:
-            for newly_available_output in newly_available_data:
-                pending_dependency_set.discard(newly_available_output)
-            
-            if len(pending_dependency_set) == 0:
-                self.runnable_list.append(blocked_rule)
-            else:
-                still_blocked_list.append(blocked_rule, pending_dependency_set)
-                
-        self.blocked_list = still_blocked_list    
+        attempt.status = TASK_ATTEMPT_STATUS_COMPLETED
+        task.status = TASK_STATUS_COMPLETED
+        worker.status = WORKER_STATUS_IDLE
+        
+        for new_output_datum in new_output_data:
+            self.session.add(new_output_datum)
+        
+#        task = self.running_tasks[task_id]
+#        del self.running_tasks[task_id]
+#        del self.busy_workers[task.worker]
+#        self.idle_workers.add(task.worker)
+#        
+#        newly_available_data = task.rule.get_data_outputs()
+#        
+#        # TODO: replace with a smarter (tree-based) lookup structure for the
+#        #       pending-dependency set.
+#        #       (e.g. a map from pending output to sets).
+#        still_blocked_list = []
+#        for (blocked_rule, pending_dependency_set) in self.blocked_list:
+#            for newly_available_output in newly_available_data:
+#                pending_dependency_set.discard(newly_available_output)
+#            
+#            if len(pending_dependency_set) == 0:
+#                self.runnable_list.append(blocked_rule)
+#            else:
+#                still_blocked_list.append(blocked_rule, pending_dependency_set)
+#                
+#        self.blocked_list = still_blocked_list    
+#    
+#        for newly_available_output in newly_available_data:
+#            self.available_data.add(newly_available_output)
     
-        for newly_available_output in newly_available_data:
-            self.available_data.add(newly_available_output)
-    
-    def assign_task(self, task):
+    def assign_task(self, attempt):
         self.idle_workers.remove(task.worker)
         self.busy_workers[task.worker] = task.id
         self.running_tasks[task.id] = task
         
-        self.bus.publish('execute_task', task)
     
     def do_schedule(self):
+        self.session.flush()
+        idle_workers = self.session.query(Worker).filter_by(status=WORKER_STATUS_IDLE)
+        runnable_tasks = self.session.query(Task).filter_by(status=TASK_STATUS_RUNNABLE)
         while not len(self.idle_workers) == 0 or len(self.runnable_list) == 0:
             # Really we should be doing Quincy or something locality-aware in here, but instead let's just do an arbitrary mapping.
-            rule = self.runnable_list.pop()
-            worker = self.idle_workers.pop()
-            task = WorkflowTask(rule, worker)
-            self.assign_task(task)
-            
-class WorkflowTask:
+            task = runnable_tasks.pop()
+            worker = idle_workers.pop()
+            attempt = TaskAttempt(task=task, worker=worker)
+            worker.status = WORKER_STATUS_BUSY
+            task.status = TASK_STATUS_RUNNING
+            self.session.add(attempt)
+            self.session.flush()
     
-    def __init__(self, rule, worker):
-        self.id = uuid4()
-        self.rule = rule
-        self.worker = worker
+            self.bus.publish('execute_task_attempt', attempt.id)
         
 class TaskExecutor(plugins.SimplePlugin):
     
@@ -213,36 +235,54 @@ class TaskExecutor(plugins.SimplePlugin):
             self.task_queue.put(THREAD_TERMINATOR)
             self.thread.join()
             
-    def execute_task(self, task):
-        self.task_queue.put(task)
+    def execute_task_attempt(self, task_attempt_id):
+        self.task_queue.put(task_attempt_id)
     
     def executor_main(self):
         http = httplib2.Http()
+        self.session = Session()
         while True:
-            task = self.task_queue.get()
-            if task is THREAD_TERMINATOR or not self.is_running:
+            task_attempt_id = self.task_queue.get()
+            if task_attempt_id is THREAD_TERMINATOR or not self.is_running:
                 break
+            
+            task_attempt = self.session.query(TaskAttempt).options(eagerload('task')).get(task_attempt_id)
             
             # Task dispatch code.
             
             # Get URL from worker pool.
-            worker_url = self.worker_pool.get_task_submission_url(task.worker)
+            worker_url = task_attempt.worker.uri
+            
+            method = task_attempt.task.method
+            args = task_attempt.task.args
+            
+            inputs = task_attempt.task.inputs
+            outputs = task_attempt.task.outputs
+            
+            arg_representations = {}
+            for input in inputs:
+                # Need to get representation as a dict.
+                arg_representations[input.id] = map(lambda x: x.as_dict(), input.representations)
+            
+            processed_outputs = []
+            for output in outputs:
+                processed_outputs.append(output.id)
             
             # Issue task creation request to worker URL.
-            request_body = simplejson.dumps({ 'master_task_id' : task.id,
-                                              'executor' : task.rule.method,
-                                              'args' : task.rule.get_args_for_submit(), 
-                                              'outputs' : task.rule.get_outputs_for_submit() })  
+            request_body = simplejson.dumps({ 'master_task_id' : task_attempt.task.id,
+                                              'master_task_attempt_id' : task_attempt.id,
+                                              'executor' : task_attempt.task.method,
+                                              'args' : args,
+                                              'arg_representations' : arg_representations,
+                                              'outputs' : processed_outputs })  
                       
             (response, content) = http.request(worker_url, 'POST', request_body)
             
             # If failed, create a failed task event!
             if response.status != 200:
-                self.bus.publish('add_failed_task', task.id)
-            
-class WorkerPool(plugins.SimplePlugin):
-    
-    def __init__(self):
-        # Map from worker-id to worker object.
-        self.workers = {}
+                self.bus.publish('add_failed_task_attempt', task_attempt.id)
+
+        self.session.close()
+        
+
         
