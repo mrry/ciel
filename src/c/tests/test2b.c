82b95c41999f0984f57080732644674f3321b647
Chris Smowton <cs448@cl.cam.ac.uk>
2010-04-26 16:40:35 +0100

    Added a C executor. Run 'make' in src/c to build it and tests.

---
+++ src/c/tests/test2b.c
@@ -0,0 +1,12 @@
+
+#include "test2.h"
+#include <stdio.h>
+
+void slave_to_nobody() {
+
+  printf("Hello, this is slave_to_nobody\n");
+  printf("Stick this up your politics and refrain from smoking it!\n");
+
+  return;
+
+}
