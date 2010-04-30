159e778d202657d61d64b8bf10e10e4d38b4ad07
Anil Madhavapeddy <anil@recoil.org>
2010-04-30 17:24:01 +0100

    chmod a+x

--- scripts/run_worker.sh
+++ scripts/run_worker.sh
@@ -4,5 +4,5 @@ PYTHON=python
 
 MASTER=${MASTER_HOST:-http://click.local:9000}
 
-export CLASSPATH=${PWD}/../java/JavaBindings.jar:$(PWD)/../sw/json_simple-1.1.jar
+export CLASSPATH=${PWD}/../src/java/JavaBindings.jar:$(PWD)/../src/sw/json_simple-1.1.jar
 ${PYTHON} ../src/python/mrry/mercator/__init__.py --role worker --master ${MASTER} --port 9001
