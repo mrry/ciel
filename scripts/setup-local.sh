50a0464070883ea1f5aab4aacee1e10a3467c26f
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2010-05-02 23:48:44 +0100

    Forced to commit :(.

--- scripts/setup-local.sh
+++ scripts/setup-local.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # pre-accept Java license agreement :)
-echo sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true | debconf-set-selections
+#echo sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true | debconf-set-selections
 
 # install stuff
 apt-get -qq -y update
-apt-get -qq -y install python python-ply python-httplib2 python-simplejson python-cherrypy3 sun-java6-jre
+apt-get -qq -y install python python-ply python-httplib2 python-simplejson
+#apt-get -qq -y install python python-ply python-httplib2 python-simplejson python-cherrypy3 sun-java6-jre
+wget http://mirrors.kernel.org/ubuntu/pool/main/p/python-support/python-support_1.0.3ubuntu1_all.deb
+dpkg -i python-support_1.0.3ubuntu1_all.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/c/cherrypy3/python-cherrypy3_3.1.2-1_all.deb
+dpkg -i python-cherrypy3_3.1.2-1_all.deb
+
