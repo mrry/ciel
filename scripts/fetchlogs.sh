d7bc51dd068a4ed503bbeb9387aaeaadcd61aba4
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2010-05-02 23:51:42 +0100

    Modified logfetcher.

--- scripts/fetchlogs.sh
+++ scripts/fetchlogs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 SWBASE="../*"
-KEY=~/.ec2/malteskey.pem
+KEY=~/.amazon/fedorakeypair.pem
 USER="root"
 
 I=0
+mkdir fetched_logs
 while read line
 do
     echo "Getting logs from instance $I"
-    scp -q -i $KEY $USER@$line:logs/error.log logs/error_$I.log
-    scp -q -i $KEY $USER@$line:logs/out.log logs/out_$I.log
+    scp -q -i $KEY $USER@$line:logs/error.log fetched_logs/error_$I.log
+    scp -q -i $KEY $USER@$line:logs/out.log fetched_logs/out_$I.log
     I=`expr $I + 1`
 done
 
