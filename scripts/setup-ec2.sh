2a89761f4e465c9eb079a8ba75dba2bb3f02eb90
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2010-05-03 12:30:07 +0100

    Updated deployment scripts.

--- scripts/setup-ec2.sh
+++ scripts/setup-ec2.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 SWBASE="../*"
-KEY=~/.amazon/fedorakeypair.pem
+if [[ $1 == "" ]]; then
+    KEY=key.pem
+else
+    KEY=$1
+fi
 USER="root"
+
+tar -cf sw-distrib.tar $SWBASE 2>/dev/null
+gzip -q sw-distrib.tar 2>/dev/null
 
 I=0
 while read line
 do
     echo -n "Setting up instance $I "
-#    scp -q -i $KEY setup-local.sh $USER@$line:
-#    scp -q -i $KEY launch-master.sh $USER@$line:
-#    scp -q -i $KEY launch-worker.sh $USER@$line:
-    scp -q -i $KEY -r $SWBASE $USER@$line:
-#    ssh -f -i $KEY $USER@$line '/root/setup-local.sh'
+    scp -q -i $KEY setup-local.sh $USER@$line:
+    scp -q -i $KEY launch-master.sh $USER@$line:
+    scp -q -i $KEY launch-worker.sh $USER@$line:
+    scp -q -i $KEY sw-distrib.tar.gz $USER@$line:
     if [ "$I" -eq "0" ]; then
 	echo "... as a master."
-	ssh -f -i $KEY $USER@$line "/root/launch-master.sh /root"
+	ssh -f -i $KEY $USER@$line "/root/setup-local.sh master /root"
 	MASTER=$line
     else
-	echo "... as a worker for $line"
-	ssh -f -i $KEY $USER@$line "/root/launch-worker.sh /root $MASTER"
+	echo "... as a worker for $MASTER"
+	ssh -f -i $KEY $USER@$line "/root/setup-local.sh worker /root $MASTER"
     fi
     I=`expr $I + 1`
 done
