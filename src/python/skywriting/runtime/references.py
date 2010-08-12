a4085968f056b2cdef2493bae71e83f5d1c65bb9
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2010-08-12 14:56:02 +0100

    Add Future2 and Concrete2 reference types.

--- src/python/skywriting/runtime/references.py
+++ src/python/skywriting/runtime/references.py
@@ -11,6 +11,7 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+import uuid
 
 '''
 Created on 20 Apr 2010
@@ -43,6 +44,61 @@ class SWNullReference(SWRealReference):
 class SWFutureReference(SWRealReference):
     pass
 
+class SWProvenance:
+    
+    def as_tuple(self):
+        pass
+    
+class SWNoProvenance(SWProvenance):
+    
+    def as_tuple(self):
+        return ('na', )
+    
+class SWTaskOutputProvenance(SWProvenance):
+    
+    def __init__(self, task_id, index):
+        self.task_id = task_id
+        self.index = index
+    
+    def as_tuple(self):
+        return ('out', str(self.task_id), self.index)
+
+class SWSpawnedTaskProvenance(SWProvenance):
+    
+    def __init__(self, task_id, spawn_list_index):
+        self.task_id = task_id
+        self.index = spawn_list_index
+
+    def as_tuple(self):
+        return ('spawn', str(self.task_id), self.index)
+
+class SWTaskContinuationProvenance(SWProvenance):
+    
+    def __init__(self, task_id):
+        self.task_id = task_id
+        
+    def as_tuple(self):
+        return ('cont', str(self.task_id))
+
+class SW2_FutureReference(SWFutureReference):
+    
+    def __init__(self, id, provenance=SWNoProvenance()):
+        self.id = id
+        self.provenance = provenance
+    
+    def as_tuple(self):
+        return ('f2', self.id, self.provenance.as_tuple())
+        
+class SW2_ConcreteReference(SWRealReference):
+        
+    def __init__(self, id, provenance, location_hints):
+        self.id = id
+        self.provenance = provenance
+        self.location_hints = location_hints
+        
+    def as_tuple(self):
+        return('c2', self.id, self.provenance.as_tuple(), self.location_hints)
+        
 class SWLocalFutureReference(SWFutureReference):
     """
     Used as a placeholder reference for the results of spawned tasks. Refers to the
@@ -128,6 +184,19 @@ class SWDataValue(SWRealReference):
     def __repr__(self):
         return 'SWDataValue(%s)' % (repr(self.value), )
 
+def build_provenance_from_tuple(provenance_tuple):
+    p_type = provenance_tuple[0]
+    if p_type == 'na':
+        return SWNoProvenance()
+    elif p_type == 'out':
+        return SWTaskOutputProvenance(uuid.UUID(hex=provenance_tuple[1]), provenance_tuple[2])
+    elif p_type == 'spawn':
+        return SWSpawnedTaskProvenance(uuid.UUID(hex=provenance_tuple[1]), provenance_tuple[2])
+    elif p_type == 'cont':
+        return SWTaskContinuationProvenance(uuid.UUID(hex=provenance_tuple[1]))
+    else:
+        raise KeyError(p_type)
+
 def build_reference_from_tuple(reference_tuple):
     ref_type = reference_tuple[0]
     if ref_type == 'urls':
@@ -148,5 +217,9 @@ def build_reference_from_tuple(reference_tuple):
         return SWErrorReference(reference_tuple[1], reference_tuple[2])
     elif ref_type == 'null':
         return SWNullReference()
+    elif ref_type == 'f2':
+        return SW2_FutureReference(uuid.UUID(hex=reference_tuple[1]), build_provenance_from_tuple(reference_tuple[2]))
+    elif ref_type == 'c2':
+        return SW2_ConcreteReference(uuid.UUID(hex=reference_tuple[1]), build_provenance_from_tuple(reference_tuple[2]), reference_tuple[3])
     else:
         raise KeyError(ref_type)
