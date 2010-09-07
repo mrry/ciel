5c2451311543da85ed3d3fbc3eecf5d7500384ba
ms705 <ms705@cl.cam.ac.uk>
2010-09-07 18:37:33 +0100

    Adding new scripts (mostly done; sw-deploy needs refactoring and the cluster scripts miss a few features)

---
+++ scripts/sw-deploy-local.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# shut up dpkg
+export DEBIAN_FRONTEND="noninteractive"
+
+# install stuff
+# XXX this requires root privileges, which the $SWUSER might not have!
+apt-get -qq -y update 1>&2 2>/dev/null
+apt-get -qq -y install git-core curl 1>&2 2>/dev/null
+
+# pre-accept Java license agreement :)
+echo sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true | debconf-set-selections
+
+# install more stuff
+apt-get -qq -y install python python-ply python-httplib2 python-simplejson python-cherrypy3 1>&2 2>/dev/null
+apt-get -qq -y install openjdk-6-jre 1>&2 2>/dev/null
+
+# git checkout
+mkdir -p /opt
+cd /opt
+git clone -q http://github.com/mrry/skywriting.git
+
