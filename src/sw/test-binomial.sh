16c93c7b54ff5c42eef55d1b2ee19dff49b912c8
Anil Madhavapeddy <anil@recoil.org>
2010-09-20 18:04:13 +0100

    add a C version of the binomial options parser

--- src/sw/test-binomial.sh
+++ src/sw/test-binomial.sh
@@ -6,22 +6,29 @@ t=1.0
 v=0.3
 rf=0.03
 cp=-1
-n=100
-chunk=10
+n=50000
+chunk=5000
+ocamlopt -unsafe -nodynlink -inline 1000 -annot -w -8 binomial_parallel.ml -o binomial-ocaml
 
-ocamlopt -g -annot -w -8 bigarray.cmxa unix.cmxa binomial_parallel.ml  -o binomial-parallel
+gcc -ffast-math -std=c99 -o binomial-c -Wall -lm -O3 binomial-parallel.c
 
 # do serial version
-echo -n 'S: '
-./binomial-serial.py $s $k $t $v $rf $cp $n
+#echo "Running: ./binomial-serial.py"
+#time ./binomial-serial.py $s $k $t $v $rf $cp $n
 
-# do parallel version
-IFS=""
-cmd="./binomial-parallel $s $k $t $v $rf $cp $n $chunk 1"
-x=$(($n+$chunk))
-while [ $x -gt 0 ]; do
-  cmd="$cmd | ./binomial-parallel $s $k $t $v $rf $cp $n $chunk 0"
-  let x-=$chunk || true
-done
-echo -n 'P: '
-bash -c "$cmd"
+function run_parallel {
+  BINP=$1
+  IFS=""
+  cmd="$BINP $s $k $t $v $rf $cp $n $chunk 1"
+  x=$(($n+$chunk))
+  while [ $x -gt 0 ]; do
+    cmd="$cmd | $BINP $s $k $t $v $rf $cp $n $chunk 0"
+    let x-=$chunk || true
+  done
+  echo "Running: $BINP"
+  time bash -c "$cmd"
+}
+
+run_parallel "./binomial-c"
+run_parallel "./binomial-ocaml"
+
