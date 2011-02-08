5b2cbc142449db8ec219868ef268343cf8a01ca5
Chris Smowton <cs448@cam.ac.uk>
2011-02-08 18:12:19 +0000

    Similarly amend gen-data to do it once

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./fetch-externals.sh
-./build-java.sh
-./gen-data.sh
-make -C src/c/src loader
-make -C src/csharp
+./fetch-externals.sh || exit
+./build-java.sh || echo "Failed to build Java bindings and examples"
+./gen-data.sh || echo "Failed to generate test data"
+make -C src/c/src loader || echo "Make C bindings failed"
+make -C src/csharp || echo "Make C# bindings failed"
