10b52355c2972c6a4dcbc11b3370350e2fd9fed4
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2011-02-15 01:47:23 +0000

    Disabled object cache, because it was leading to aliasing issues in the following case:
    1. Task A runs and spawns a continuation B, storing the continuation object in
       the cache.
    
    2. Task B runs on the same worker, loading its continuation object from the
       cache, without taking a deep copy.
    
    3. Task A runs again for some reason, and finds that its continuation table has
       been changed, leading to a circular dependency.

--- src/python/skywriting/runtime/block_store.py
+++ src/python/skywriting/runtime/block_store.py
@@ -977,7 +977,7 @@ class BlockStore(plugins.SimplePlugin):
     
     def store_object(self, object, encoder, id):
         """Stores the given object as a block, and returns a swbs URL to it."""
-        self.object_cache[id] = object
+        #self.object_cache[id] = object
         with open(self.filename(id), "wb") as object_file:
             self.encoders[encoder](object, object_file)
             file_size = object_file.tell()
