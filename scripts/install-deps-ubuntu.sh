6cf9b0c59fc1168f3da18da5c836e4412ce8638a
Chris Smowton <cs448@cam.ac.uk>
2011-02-15 17:06:47 +0000

    Add Mono dep

--- scripts/install-deps-ubuntu.sh
+++ scripts/install-deps-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # list of packages to be installed (space-delimited)
-PACKAGES="python python-ply python-httplib2 python-simplejson python-cherrypy3 python-pycurl curl lighttpd python-flup ant openjdk-6-jdk mono-devel"
+PACKAGES="python python-ply python-httplib2 python-simplejson python-cherrypy3 python-pycurl curl lighttpd python-flup ant openjdk-6-jdk mono-devel mono-mcs"
 
 # the JDK to install
 JDK="openjdk-6-jre"
@@ -14,3 +14,8 @@ echo sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true | debconf-set-selec
 
 apt-get -qq -y install $PACKAGES 1>&2 2>/dev/null
 apt-get -qq -y install $JDK 1>&2 2>/dev/null
+
+# Because the Debian defaults assume we actually want this thing as our default server...
+
+update-rc.d lighttpd disable
+service lighttpd stop
