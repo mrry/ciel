1266de044efef211c178589e39ddd039b5dc0041
Anil Madhavapeddy <anil@recoil.org>
2011-04-24 18:38:41 +0100

    portability fixes for MacOS X for run_master: do not depend on readlink -f (not present on MacOS X), check for lighttpd before using it on the command line

--- scripts/run_master.sh
+++ scripts/run_master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BASE=$(dirname $(readlink -f $0))/..
+BASE=$(python -c "import os,sys;print os.path.dirname(os.path.realpath('$0'))")/..
 export PYTHONPATH=$PYTHONPATH:$BASE/src/python
 PYTHON=python
 
@@ -12,4 +12,17 @@ if [[ $REL_BLOCK_LOCATION == "" ]]; then
     REL_BLOCK_LOCATION="store/"
 fi
 
-${PYTHON} $BASE/src/python/skywriting/__init__.py --role master --port $MASTER_PORT --staticbase $BASE/src/js/skyweb/ --lighttpd-conf $BASE/src/python/skywriting/runtime/lighttpd.conf -j $BASE/journal/ -b $BASE/$REL_BLOCK_LOCATION -T ciel-process-aaca0f5eb4d2d98a6ce6dffa99f8254b $*
+ABS_BLOCK_LOCATION="$BASE/$REL_BLOCK_LOCATION"
+
+if [ ! -d "$ABS_BLOCK_LOCATION" ]; then
+  mkdir -p "$ABS_BLOCK_LOCATION"
+fi
+
+LIGHTTPD_BIN=`which lighttpd`
+if [ "$LIGHTTPD_BIN" = "" ]; then
+  HTTPD=
+else
+  HTTPD="--lighttpd-conf $BASE/src/python/skywriting/runtime/lighttpd.conf"
+fi
+
+${PYTHON} "$BASE/src/python/skywriting/__init__.py" --role master --port $MASTER_PORT --staticbase "$BASE/src/js/skyweb/" -j $BASE/journal/ -b "$ABS_BLOCK_LOCATION" -T ciel-process-aaca0f5eb4d2d98a6ce6dffa99f8254b $*
