4082bd8651f8e7eb410ba58b6ff9aa991c9d755f
Anil Madhavapeddy <anil@recoil.org>
2011-04-28 23:10:39 +0100

    add a wrapper to run all the remote variants

--- src/sw/remote-binomial.sh
+++ src/sw/remote-binomial.sh
@@ -6,22 +6,24 @@ t=1.0
 v=0.3
 rf=0.03
 cp=-1
-n=50000
-chunk=5000
+n=800000
+chunk=40000
 
 total=$(( ($n / $chunk) + 1 ))
 num=$1
-dir="$2"
-cmd="$3"
+shift
+dir="$1"
+shift
+cmd="$*"
 script="$(basename $0)"
-fullcmd="./$cmd $s $k $t $v $rf $cp $n $chunk"
+fullcmd="$cmd $s $k $t $v $rf $cp $n $chunk"
 HOSTS=( $(cat hosts.txt) )
 if [ $num -eq $total ]; then
-  ./$fullcmd 0
+  $fullcmd 0
 elif [ $num -eq 0 ]; then
   let num+=1
-  ./$fullcmd 1 | ssh -A ${HOSTS[$num]} "cd $dir && ./$script $num $dir $cmd"
+  $fullcmd 1 | ssh -c blowfish -A ${HOSTS[$num]} "cd $dir && ./$script $num $dir $cmd"
 else
   let num+=1
-  ./$fullcmd 0 | ssh -A ${HOSTS[$num]} "cd $dir && ./$script $num $dir $cmd"
+  $fullcmd 0 | ssh -c blowfish -A ${HOSTS[$num]} "cd $dir && ./$script $num $dir $cmd"
 fi
