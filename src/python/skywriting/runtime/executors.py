12792706238e9f9ae46f2aaed5179757c72ccc5e
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2011-05-28 11:12:57 +0000

    Removed band-aid fix to sweetheart-making (solution is to close inputs properly!).

--- src/python/skywriting/runtime/executors.py
+++ src/python/skywriting/runtime/executors.py
@@ -634,11 +634,9 @@ class ProcExecutor(BaseExecutor):
         else:
             ctx = retrieve_file_or_string_for_ref(ref, self.task_record)
         if ctx.completed_ref is not None:
-            #if make_sweetheart:
-            #    ctx.completed_ref = SW2_SweetheartReference.from_concrete(ctx.completed_ref, get_own_netloc())
+            if make_sweetheart:
+                ctx.completed_ref = SW2_SweetheartReference.from_concrete(ctx.completed_ref, get_own_netloc())
             self.task_record.publish_ref(ctx.completed_ref)
-        if make_sweetheart:
-            self.task_record.publish_ref(SW2_SweetheartReference(ref.id, get_own_netloc()))
         return ctx.to_safe_dict()
         
     def publish_fetched_ref(self, fetch):
@@ -679,10 +677,6 @@ class ProcExecutor(BaseExecutor):
         # "blocking" means that EOF, as and when it arrives, means what it says. i.e. it's a regular file and done, or a pipe-like thing.
         ret.update({"done": new_fetch.done, "size": new_fetch.bytes})
         ciel.log("Async fetch %s (chunk %d): initial status %d bytes, done=%s, blocking=%s, sending_fd=%s" % (real_ref, chunk_size, ret["size"], ret["done"], ret["blocking"], ret["sending_fd"]), "EXEC", logging.INFO)
-
-        # XXX: adding this because the OngoingFetch isn't publishing the sweetheart correctly.        
-        if make_sweetheart:
-            self.task_record.publish_ref(SW2_SweetheartReference(ref.id, get_own_netloc()))
 
         if new_fetch.done:
             if not new_fetch.success:
