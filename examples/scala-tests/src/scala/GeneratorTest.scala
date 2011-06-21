9d4d0edc601c4dc6153c9f07f5076e3e0b3019f0
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2011-06-21 03:36:52 +0100

    Additional Skyla tests.

--- examples/scala-tests/src/scala/GeneratorTest.scala
+++ examples/scala-tests/src/scala/GeneratorTest.scala
@@ -4,21 +4,17 @@ import com.asgow.ciel.scala._
 import com.asgow.ciel.references._
 
 class ScalaGeneratorTest extends SkylaThread[Int] {
-
   override def run = {
 
-    val a = Skyla.spawnGenerator[Int] { cyield =>
-      cyield(5)
-      cyield(6)
-      cyield(7)
+    val a = Skyla.spawnGenerator[Int] { _yield =>
+      _yield(6)
+      _yield(3)
+      _yield(7)
     }
 
-    val iter = a.iterator
+    val iter = a.iterator 
+    val total = iter.sum
 
-    println(iter.sum)
-
-    5
-
+    total
   }
-
 }
