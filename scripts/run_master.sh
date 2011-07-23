c8b4b958ded407fff565876e3a69efe13eb02ee4
Steven Smith <sos22@srcf.ucam.org>
2011-07-23 21:09:51 +0100

    Start trying to implement a make install target for ciel.  This is enough that install completes without errors and run_master.sh works for either installed or in-place execution in the source directory.

--- scripts/run_master.sh
+++ scripts/run_master.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-PYTHON=${PYTHON:-python}
-BASE=$(${PYTHON} -c "import os,sys;print os.path.dirname(os.path.realpath('$0'))")/..
-export PYTHONPATH=$PYTHONPATH:$BASE/src/python
 
 # Sensible defaults:
 if [[ $MASTER_PORT == "" ]]; then
@@ -13,6 +10,42 @@ if [[ $REL_BLOCK_LOCATION == "" ]]; then
 fi
 
 
+if [ "${0#/}" = "${0}" ]
+then
+    me=$(pwd)/$0
+else
+    me=$0
+fi
+
+# Figure out whether this is running in-place in the source directory
+# or in an installed version
+install_prefix=${me%bin/run_master.sh}
+if [ "$install_prefix" = "$me" ]
+then
+    # It's a source install
+    BASE=$(pwd)
+    my_python_path="$BASE/src/python"
+    sw_master=${BASE}/scripts/sw-master
+else
+    # Running from an installed version
+    if [ "$install_prefix" != "/" ] && [ "$install_prefix" != "/usr" ] && [ "$install_prefix" != "/usr/local" ]
+    then
+	PYTHONVER=$(python --version 2>&1 | cut -d' ' -f 2 | cut -d'.' -f 1,2)
+	my_python_path=$install_prefix/lib/python${PYTHONVER}/site-packages
+    else
+	my_python_path=""
+    fi
+    sw_master=${install_prefix}/bin/sw-master
+fi
+if ! [ -z "$my_python_path" ]
+then
+    if [ -z "$PYTHONPATH" ]
+    then
+	export PYTHONPATH=$my_python_path
+    else
+	PYTHONPATH=${PYTHONPATH}:$my_python_path
+    fi
+fi
 
 ABS_BLOCK_LOCATION="$BASE/$REL_BLOCK_LOCATION"
 
@@ -25,4 +58,4 @@ if [ "$LIGHTTPD_BIN" != "" ]; then
   EXTRA_CONF="${EXTRA_CONF} --lighttpd-conf $BASE/src/python/skywriting/runtime/lighttpd.conf"
 fi
 
-${PYTHON} "$BASE/src/python/skywriting/__init__.py" --role master --port $MASTER_PORT --staticbase "$BASE/src/js/skyweb/" -b "$ABS_BLOCK_LOCATION" -T ciel-process-aaca0f5eb4d2d98a6ce6dffa99f8254b ${EXTRA_CONF} $*
+${sw_master} --role master --port $MASTER_PORT --staticbase "$BASE/src/js/skyweb/" -b "$ABS_BLOCK_LOCATION" -T ciel-process-aaca0f5eb4d2d98a6ce6dffa99f8254b ${EXTRA_CONF} $*
