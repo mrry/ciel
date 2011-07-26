46334b3b34853a4bf6814d6b05556ffc8d5b2ad6
Anil Madhavapeddy <anil@recoil.org>
2011-07-26 16:22:29 +0100

    update setup.py with more good stuff

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
 from setuptools import setup
 
 setup(
-    name = "skywriting",
+    name = "ciel",
     version = '0.1-dev',
-    description = "Programming language for distributed, parallel computation",
+    description = "Execution engine for distributed, parallel computation",
     author = "Derek Murray",
     author_email = "derek.murray@cl.cam.ac.uk",
-    url = "http://www.cl.cam.ac.uk/research/srg/netos/skywriting/",
+    url = "http://www.cl.cam.ac.uk/netos/ciel/",
     packages = [ 'skywriting', 'skywriting.lang', 'skywriting.runtime',
                  'skywriting.runtime.master', 'skywriting.runtime.worker',
                  'skywriting.runtime.util', 'ciel', 'shared' ],
     package_dir = { '' : 'src/python' },
-    scripts = [ 'scripts/sw-master', 'scripts/sw-worker', 'scripts/sw-job',
-                'scripts/sw-console', 'scripts/run_master.sh',
-                'scripts/run_worker.sh', 'scripts/sw-start-job',
-                'scripts/run_job.sh' ],
-    data_files = [ ("share/ciel/skyweb", ['src/js/skyweb/%s' % s
-                                          for s in
-                                          ['jquery-1.4.2.js',
-                                           'jquery.query-2.1.7.js',
-                                           'raphael-min.js',
-                                           'skyweb.css',
-                                           'skyweb.js',
-                                           'skyweb-test.html']] ),
-                   ("share/ciel", ["src/python/skywriting/runtime/lighttpd.conf"]),
+    scripts = [ "scripts/%s" %s for s in
+                  ['ciel-killall.sh', 'ciel-kill-cluster', 'ciel-launch-cluster',
+                   'ciel-launch-master', 'ciel-launch-worker', 'ciel-poll-job',
+                   'ciel-print-job-result', 'ciel-run-job', 'ciel-run-job-async',
+                   'ciel-task-crawler', 'sw-job', 'sw-master', 'sw-start-job', 
+                   'sw-worker'] ],
+    data_files = [ ("share/ciel", ["src/python/skywriting/runtime/lighttpd.conf"]),
                    ("share/ciel/skywriting",
                     ["src/sw/stdlib/%s" %s for s in
                      ["environ", "grab", "java", "mapreduce",
@@ -37,6 +31,6 @@ setup(
             'Topic :: Software Development :: Interpreters',
             'Topic :: System :: Distributed Computing',
         ],
-    requires=['simplejson', 'CherryPy (>=3.1.0)' ]
+    requires=['simplejson', 'CherryPy (>=3.1.0)', 'ply', 'pycurl', 'httplib2' ]
 )
 
