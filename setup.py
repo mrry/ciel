6e8193993776063197bad75ef1a37757bb78a6be
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2011-08-16 16:12:04 +0100

    Tweaked the setup.py file.

--- setup.py
+++ setup.py
@@ -18,13 +18,17 @@ setup(
 #                   'ciel-task-crawler', 'sw-job', 'sw-start-job' ] ],
     classifiers = [
             'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Environment :: Web Environment',
             'Intended Audience :: Developers',
+            'Intended Audience :: Information Technology',
             'Intended Audience :: Science/Research',
+            'Intended Audience :: System Administrators',
             'License :: OSI Approved :: ISC License (ISCL)',
             'Operating System :: POSIX',
-            'Topic :: Software Development :: Interpreters',
+            'Topic :: System :: Clustering',
             'Topic :: System :: Distributed Computing',
         ],
-    requires=['simplejson', 'CherryPy (>=3.1.0)', 'ply', 'pycurl', 'httplib2' ]
+    requires=['simplejson', 'CherryPy (>=3.1.0)', 'pycurl', 'httplib2' ]
 )
 
